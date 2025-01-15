739a4db478056aaa61fd122ac125cf8476f44886
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-01-15 17:55:55 +0100

    added api-services function to handle diffrent api calls to backend

---
+++ src/services/apiServices.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000";
+
+const apiServices = {
+  get: async (endpoint: string, params?: Record<string, any>) => {
+    const response = await axios.get(`${BASE_URL}${endpoint}`, { params });
+    return response.data;
+  },
+  post: async (endpoint: string, data: any) => {
+    const response = await axios.post(`${URL}${endpoint}`, data);
+    return response.data;
+  },
+  put: async (endpoint: string, data: any) => {
+    const response = await axios.put(`${BASE_URL}${endpoint}`, data);
+    return response.data;
+  },
+  delete: async (endpoint: string) => {
+    const response = await axios.delete(`${BASE_URL}${endpoint}`);
+    return response.data;
+  },
+};
+
+export default apiServices;
