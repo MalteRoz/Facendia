87ac1eedb31e95bb7606e2d9830a91bad5c994f5
Malte Rozenbeek <malte.rozenbeek@gmail.com>
2025-01-20 19:53:27 +0100

    checkup modal added if users wants to delete a task, this ensures that users dont missclick and lose the task. It will show more correct data on the dashboard

--- src/components/DisplayTask.tsx
+++ src/components/DisplayTask.tsx
@@ -4,7 +4,6 @@ import { Task } from "../models/ITask";
 import "./scss/_displayTask.scss";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { FaRegCheckCircle } from "react-icons/fa";
-import { IoIosArrowDown } from "react-icons/io";
 import { HiMiniArrowDown } from "react-icons/hi2";
 
 const DisplayTask = ({
@@ -15,9 +14,14 @@ const DisplayTask = ({
   setData: React.Dispatch<React.SetStateAction<Task[]>>;
 }) => {
   //DELETE metod för att ta bort task från DB
-  //göra funktion som hanterar click på soptunnan
-  //apiServices.delete
+  //göra funktion som hanterar click på soptunnan -> klar
+  //apiServices.delete -> klar
   //modal som popar upp och frågar om man verkligen vill ta bort tasken
+  //useState som kontrollerar ifall showModal är true eller inte -> klar
+  //visar popUpmodal
+
+  const [showPopUp, setShowPopUp] = useState(false);
+  const [selectedTaskId, setSelectedTaskId] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,6 +34,11 @@ const DisplayTask = ({
     };
     fetchData();
   }, []);
+
+  const handleDeleteCheck = (id: number) => {
+    setShowPopUp(true);
+    setSelectedTaskId(id);
+  };
 
   const handleDeleteTask = async (id: number) => {
     console.log(id);
@@ -62,7 +71,7 @@ const DisplayTask = ({
                 <div className="icon-container">
                   <FaRegTrashCan
                     size={16}
-                    onClick={() => handleDeleteTask(task.id)}
+                    onClick={() => handleDeleteCheck(task.id)}
                   />
                   <FaRegCheckCircle size={16} />
                 </div>
@@ -73,6 +82,34 @@ const DisplayTask = ({
               </div>
             </div>
           ))}
+
+          {showPopUp && (
+            <div className="overlay">
+              <div className="popUp-container">
+                <p>
+                  Do you want to{" "}
+                  <span style={{ color: "red", fontWeight: "600" }}>
+                    delete
+                  </span>{" "}
+                  this task?
+                </p>
+                <div className="popUp-button-container">
+                  <button
+                    onClick={() => {
+                      if (selectedTaskId !== null) {
+                        handleDeleteTask(selectedTaskId);
+                      }
+                      setSelectedTaskId(0);
+                      setShowPopUp(false);
+                    }}
+                  >
+                    Yes
+                  </button>
+                  <button onClick={() => setShowPopUp(false)}>No, close</button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </div>
